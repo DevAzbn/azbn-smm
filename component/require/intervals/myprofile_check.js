d4fd2d17d67f6aaf2f0c02296d07bdd07dabbcc0
Alexander <alexander.zybin@gmail.com>
2016-08-24 15:24:11 +0300

    201608241524

--- component/require/intervals/myprofile_check.js
+++ component/require/intervals/myprofile_check.js
@@ -41,6 +41,8 @@ function _(azbn) {
 						
 						var vk = require('./../../vk')(azbn, h.app_id);
 						
+						vk.setToken(h.access_token);
+						
 						vk.request('users.get', {'user_ids' : h.user_id, fields :'photo_id,verified,sex,bdate,city,country,home_town,has_photo,photo_50,photo_100,photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,domain,has_mobile,contacts,site,education,universities,schools,status,last_seen,followers_count,occupation,nickname,relatives,relation,personal,connections,exports,wall_comments,activities,interests,music,movies,tv,books,games,about,quotes,can_post,can_see_all_posts,can_see_audio,can_write_private_message,can_send_friend_request,is_favorite,is_hidden_from_feed,timezone,screen_name,maiden_name,crop_photo,is_friend,friend_status,career,military,counters' }, function(resp) {
 							
 							if(azbn.is_def(resp.error) && !azbn.is_null(resp.error)) {
