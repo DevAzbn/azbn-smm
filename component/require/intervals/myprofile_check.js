5ba81907dd377445a9b151fe32b9bd57957fb08c
Alexander <alexander.zybin@gmail.com>
2016-08-31 17:08:25 +0300

    201608311708

--- component/require/intervals/myprofile_check.js
+++ component/require/intervals/myprofile_check.js
@@ -45,7 +45,7 @@ function _(azbn) {
 						
 						var __req = {'user_ids' : h.user_id, fields :'photo_id,verified,sex,bdate,city,country,home_town,has_photo,photo_50,photo_100,photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,domain,has_mobile,contacts,site,education,universities,schools,status,last_seen,followers_count,occupation,nickname,relatives,relation,personal,connections,exports,wall_comments,activities,interests,music,movies,tv,books,games,about,quotes,can_post,can_see_all_posts,can_see_audio,can_write_private_message,can_send_friend_request,is_favorite,is_hidden_from_feed,timezone,screen_name,maiden_name,crop_photo,is_friend,friend_status,career,military,counters' };
 						
-						vk.request('users.get', , function(resp) {
+						vk.request('users.get', __req, function(resp) {
 							
 							azbn.mdl('nedb.log').insert({
 								created_at : azbn.now(),
